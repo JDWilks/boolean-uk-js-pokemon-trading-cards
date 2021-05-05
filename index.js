f26c489c3144edc51ef4cfe0ac571e9c226b5174
Jonathon Wilks <jonathondwilks@icloud.com>
2021-05-05 14:19:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// write your code here!
+// This variable has the data you're working with
+
+console.log(data);
+
+
+// my code start
+
+//Create a card using JS that represents a single pokemon
+
+// card has title
+// card has image
+// card has info thats in the console.log(data) provided
+// i've got no idea at all 
+
+const h2El = document.createElement('h2')
+document.body.append(h2El)
+h2El = "Card Name"
+
+
+// - Use the exact CSS classes you see in the example HTML to obtain the same 
+// style for each card
+
+
+// - The cards should be nested inside <section class="cards"></section>
+
+// - Use the official-artwork object key as the images for the card. 
+// The images are all inside of the sprites key, in each pokemon object
+
+// - Render all the cards on the page that represents all the pokemons, 
+// recreating the same layout, using J
