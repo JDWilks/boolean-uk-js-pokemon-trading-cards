dec81b24d0e67133f68f71e55ace987fed7be633
Jonathon Wilks <jonathondwilks@icloud.com>
2021-05-06 07:58:05 +0100

    disaster ! images not showing up and now i have the cards twice !

--- index.js
+++ index.js
@@ -1,31 +1,109 @@
-// write your code here!
-// This variable has the data you're working with
+// // write your code here!
+// // This variable has the data you're working with
 
-console.log(data);
-
-
-// my code start
-
-//Create a card using JS that represents a single pokemon
-
-// card has title
-// card has image
-// card has info thats in the console.log(data) provided
-// i've got no idea at all 
-
-const h2El = document.createElement('h2')
-document.body.append(h2El)
-h2El = "Card Name"
+// console.log(data);
 
 
-// - Use the exact CSS classes you see in the example HTML to obtain the same 
-// style for each card
+// // my code start
+
+// // created an article element with the class of card
+
+// const article = document.createElement("article")
+// article.className = "card"
+
+// console.log(article)
+
+// // created a title element with the class of card-title
+
+// const title = document.createElement("h2")
+// title.className = "card--title"
+
+// console.log(title)
+
+// // added the title (h2) to the article (nesting it)
+
+// article.append(title)
+
+// // found the section with a class of cards and assigned to 
+// // variable cardsSection
+
+// const cardsSection = document.querySelector(".cards")
+
+// // added the article to cardsSection (cards)
+
+// cardsSection.append(article)
+
+// //
+
+// ****************************************************
+
+// using the above code which is in the correct structure
+// this works for names
+// creating a loop to put in the names
+
+// for(const pokemon of data){
+   
+//     createCard(pokemon.name)
+// }
+
+// // creating a function
+
+// function createCard(pokemonName){
+// const article = document.createElement("article")
+// article.className = "card"
+
+// const title = document.createElement("h2")
+// title.className = "card--title"
+
+// title.innerText = pokemonName
+
+// article.append(title)
+
+// const cardsSection = document.querySelector(".cards")
+
+// cardsSection.append(article)
+
+// }
+
+// ****************************************************
+
+// /// adding to the above to include images // 
 
 
-// - The cards should be nested inside <section class="cards"></section>
+for(const pokemon of data){
+   
+    createCard(pokemon.name) 
+}
 
-// - Use the official-artwork object key as the images for the card. 
-// The images are all inside of the sprites key, in each pokemon object
+for(const pokemonImg of data){
+   
+    createCard(pokemonImg.sprites.other['official-artwork'].front_default)
+}
 
-// - Render all the cards on the page that represents all the pokemons, 
-// recreating the same layout, using J
+// creating a function
+
+function createCard(pokemonName, pokemonImg){
+
+const article = document.createElement("article")
+article.className = "card"
+
+const title = document.createElement("h2")
+title.className = "card--title"
+
+article.append(title)
+
+title.innerText = pokemonName
+
+const pokemonImage = document.createElement("img")
+pokemonImage.className = "card--img"
+    
+article.append(pokemonImage)
+    
+pokemonImage.src = pokemonImg
+
+const cardsSection = document.querySelector(".cards")
+
+cardsSection.append(article)
+
+}
+
